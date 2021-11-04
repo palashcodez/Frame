19037973f0109633ef054c7bcb826156937a44c5
palashcodez <82249617+palashcodez@users.noreply.github.com>
2021-11-03 20:09:32 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+function preload(){
+
+}
+
+//let capture;
+
+function setup(){
+    canvas=createCanvas(600, 600);
+    canvas.position(650, 300);
+
+    video = createCapture(VIDEO);
+    video.size(400, 450);
+    video.hide();
+
+}
+
+
+function draw(){
+    background(255);   
+    stroke(0, 243, 123);   
+    strokeWeight(20);       
+    fill(150);          
+    rect( 90, 65, 420, 470); 
+
+    image(video, 90, 65, 420, 470);
+}
+
+
+
+function TakeSnapshot(){
+    save("Frameanimationimage.png");
+}
+
+
+
+
